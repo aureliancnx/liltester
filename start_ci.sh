99844af6ad974a89f955bb334d83789bfe17adfc
Aurelian <aureliancnx@gmail.com>
2021-03-08 00:10:50 +0100

    test ci

--- start_ci.sh
+++ start_ci.sh
@@ -3,4 +3,4 @@ git clone https://github.com/aureliancnx/liltester.git
 
 # Move sources to main folder
 mv liltester/src/* .
-python3 ../liltester.py -it $LILTESTER_NAME
+python3 liltester.py -it $LILTESTER_NAME
